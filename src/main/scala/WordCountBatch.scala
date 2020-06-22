65ca96d5304edbaf11dd1e99f28ac10d9ebd98b0
wuran01 <wuran01@ky-tech.com.cn>
2020-06-22 19:17:29 +0800

    init  入参

--- src/main/scala/WordCountBatch.scala
+++ src/main/scala/WordCountBatch.scala
@@ -1,21 +1,38 @@
-import org.apache.flink.api.scala.ExecutionEnvironment
-object WordCountBatch {
-  def main(args: Array[String]): Unit = {
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.api.scala._
 
-    val inputPath = "D:\\data\\11.txt"
-    //    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val env = ExecutionEnvironment.getExecutionEnvironment
-    //val env = ExecutionEnvironment.createLocalEnvironmentWithWebUI()
-    import  org.apache.flink.api.scala._
-    val text = env.readTextFile(inputPath)
-    val counts = text.flatMap(_.split("\\W+"))
-      .filter(_.nonEmpty)
-      .map((_,1))
+object WordCountBatch {
+
+  def main(args: Array[String]) {
+
+    val params: ParameterTool = ParameterTool.fromArgs(args)
+
+    // set up execution environment
+    //val env = ExecutionEnvironment.getExecutionEnvironmen
+    val  env =ExecutionEnvironment.createLocalEnvironment(2)
+    // make parameters available in the web interface
+    env.getConfig.setGlobalJobParameters(params)
+    val text =
+      if (params.has("input")) {
+        env.readTextFile(params.get("input"))
+      } else {
+        println("Executing WordCount example with default input data set.")
+        println("Use --input to specify file input.")
+        env.readTextFile("D:\\data\\11.txt")
+      }
+
+    val counts = text.flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }
+      .map { (_, 1) }
       .groupBy(0)
       .sum(1)
 
-    counts.writeAsCsv("D:\\data\\output6").setParallelism(1)
-    env.execute("batch wordCount")
-  }
+    if (params.has("output")) {
+      counts.writeAsCsv(params.get("output"), "\n", " ")
+      env.execute("Scala WordCount Example")
+    } else {
+      println("Printing result to stdout. Use --output to specify output path.")
+      counts.print()
+    }
 
+  }
 }
